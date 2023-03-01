f0e25055d1fbc18b866630adeaa8c48f7a8bed37
Alexey-Romaniv <105770723+Alexey-Romaniv@users.noreply.github.com>
2023-03-01 13:23:50 +0100

    fix users.js ctrl

--- controllers/users.js
+++ controllers/users.js
@@ -1,83 +1,82 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const { User } = require("../models/user");
-const { ctrlWrapper, HttpError } = require("../helpers/index");
+const {User} = require("../models/user");
+const {ctrlWrapper, HttpError} = require("../helpers/index");
 
 require("dotenv").config();
 const secret = process.env.SECRET_KEY;
 
 const register = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
 
-  if (user) {
-    throw HttpError(409, "Email in use");
-  }
-  const hashPassword = await bcrypt.hash(password, 10);
+    if (user) {
+        throw HttpError(409, "Email in use");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({
-    ...req.body,
-    password: hashPassword,
-  });
+    const newUser = await User.create({
+        ...req.body,
+        password: hashPassword,
+    });
 
-  const id = newUser._id;
-  const token = jwt.sign({ id }, secret, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(id, { token });
+    const id = newUser._id;
+    const token = jwt.sign({id}, secret, {expiresIn: "23h"});
+    await User.findByIdAndUpdate(id, {token});
 
-  res.status(201).json({
-    token,
-    user: {
-      email: newUser.email,
-      subscription: newUser.subscription,
-    },
-  });
+    res.status(201).json({
+        token,
+        user: {
+            email
+        },
+    });
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+    const {email, password} = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({email});
 
-  if (!user) {
-    throw HttpError(401, "Email or password is wrong");
-  }
+    if (!user) {
+        throw HttpError(401, "Email or password is wrong");
+    }
 
-  const compareResult = await bcrypt.compare(password, user.password);
+    const compareResult = await bcrypt.compare(password, user.password);
 
-  if (!compareResult) {
-    throw HttpError(401, "Email or password is wrong");
-  }
+    if (!compareResult) {
+        throw HttpError(401, "Email or password is wrong");
+    }
 
-  const id = user._id;
-  const token = jwt.sign({ id }, secret, { expiresIn: "23h" });
-  await User.findByIdAndUpdate(id, { token });
-  res.status(201).json({
-    token,
-    user: {
-      email: user.email,
-    },
-  });
+    const id = user._id;
+    const token = jwt.sign({id}, secret, {expiresIn: "23h"});
+    await User.findByIdAndUpdate(id, {token});
+    res.status(201).json({
+        token,
+        user: {
+            email: user.email,
+        },
+    });
 };
 
 const logout = async (req, res) => {
-  const { _id } = req.user;
+    const {_id} = req.user;
 
-  await User.findByIdAndUpdate(_id, { token: "" });
-  res.status(204).end();
+    await User.findByIdAndUpdate(_id, {token: ""});
+    res.status(204).end();
 };
 const getCurrent = async (req, res) => {
-  const { email } = req.user;
-  res.json({
-    user: {
-      email,
-    },
-  });
+    const {email} = req.user;
+    res.json({
+        user: {
+            email,
+        },
+    });
 };
 
 module.exports = {
-  register: ctrlWrapper(register),
-  login: ctrlWrapper(login),
-  getCurrent: ctrlWrapper(getCurrent),
-  logout: ctrlWrapper(logout),
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
+    getCurrent: ctrlWrapper(getCurrent),
+    logout: ctrlWrapper(logout),
 };
